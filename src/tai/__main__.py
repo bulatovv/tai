e3c0bb6586eaffb747a97e58aa936ab0fe353c2b
Albert Fazlyev <albert.fz@yandex.ru>
2025-08-06 01:24:05 +0500

    add app entrypoint

---
+++ src/tai/__main__.py
@@ -0,0 +1,16 @@
+import duckdb
+import trio
+
+from tai.collection import collect_sessions
+from tai.database import init_db
+
+
+async def main():
+    """Module entrypoint"""
+    init_db()
+    async with trio.open_nursery() as n:
+        with duckdb.connect('database.db') as con:
+            n.start_soon(collect_sessions, con)
+
+
+trio.run(main)
